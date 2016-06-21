2d57ba131079e314cbd67c6f16b2cb68701a7ba1
Spyke <spyke.coder@gmail.com>
2016-06-21 22:53:43 +0300

    ## WIP - Настройка конфигурации DEV

--- configs/dev.php
+++ configs/dev.php
@@ -147,5 +147,5 @@ $config = [
     $config['modules']['gii'] = 'yii\gii\Module';
 }*/
 
-$localDb = Yii::getAlias('@app/../../localdb.php');
+$localDb = \Yii::getAlias('@app/../../localdb.php');
 return \yii\helpers\ArrayHelper::merge($config, require($localDb));
